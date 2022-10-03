49fbca93d9f1f47ccbf4f671ac3ed738cb2ff85f
Adarsh Singh <asingh1058@myseneca.ca>
2022-10-02 23:51:49 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+
+var HTTP_PORT = process.env.PORT || 8080;
+
+app.use(express.static("static"));
+
+
+app.get("/registration", function (req, res) {
+  res.sendFile(path.join(__dirname, "registration.html"));
+});
+
+app.get("/login", function (req, res) {
+    res.sendFile(path.join(__dirname, "login.html"));
+  });
+app.listen(HTTP_PORT);
